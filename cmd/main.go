ce194749171c4528cb1454e3c7c0cfd10ba8536d
Christhian Guevara <christhianjesus@live.com>
2022-12-13 11:57:07 -0500

    Added songs service with tests

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"christhianguevara/songs-search-exercise/cmd/config"
 	"christhianguevara/songs-search-exercise/domain/constants"
-	"christhianguevara/songs-search-exercise/domain/entities"
 	"christhianguevara/songs-search-exercise/internal/handlers"
 	"christhianguevara/songs-search-exercise/internal/middlewares"
-	"christhianguevara/songs-search-exercise/internal/tests/mocks"
+	"christhianguevara/songs-search-exercise/internal/services"
 	"fmt"
 
 	"github.com/joeshaw/envdecode"
 	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/mock"
 )
 
 func main() {
@@ -30,14 +28,11 @@ func main() {
 }
 
 func setupHandlers(router *echo.Group) {
-	// Mock
-	songsServiceMock := new(mocks.SongsService)
-	songsServiceMock.
-		On("Search", mock.Anything, mock.Anything).
-		Return([]entities.Song{{ID: "1"}}, nil)
+	// Services
+	songsService := services.NewSongsService(nil)
 
 	// Handlers
-	songsHandler := handlers.NewSongsHandler(songsServiceMock)
+	songsHandler := handlers.NewSongsHandler(songsService)
 
 	// Register routes
 	songsHandler.RegisterRoutes(router)
